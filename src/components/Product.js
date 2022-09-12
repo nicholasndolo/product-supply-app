655548821c9b96b259074eb64b1a3f0c5d566d66
Nicholas <nicholas.nmalinda@gmail.com>
2022-09-12 10:17:17 +0300

    added handleDelete function in the products component

--- src/components/Product.js
+++ src/components/Product.js
@@ -15,23 +15,23 @@ function Product({productId, productImage, productName, productPrice,productList
       
       })
     }
-//   return (
-//     <div className="col">
-//       <div className="card h-90" style={{ width: 18 + "rem" }}>
-//         <img src={productImage} style={{ height: 200 + 'px', width: 286 + 'px' }} className="card-img-top" alt="" />
-//         <div className="card-body">
-//           <h5 className="card-title">{productName}</h5>
-//           <Link to={"/products/" + productId} className="btn btn-primary">
-//             View Details
-//           </Link>
-//         </div>
-//         <div className="card-footer">
-//           <button className="btn btn-danger mt-1" onClick={handleDelete}>Delete</button>
+  return (
+    <div className="col">
+      <div className="card h-90" style={{ width: 18 + "rem" }}>
+        <img src={productImage} style={{ height: 200 + 'px', width: 286 + 'px' }} className="card-img-top" alt="" />
+        <div className="card-body">
+          <h5 className="card-title">{productName}</h5>
+          <Link to={"/products/" + productId} className="btn btn-primary">
+            View Details
+          </Link>
+        </div>
+        <div className="card-footer">
+          <button className="btn btn-danger mt-1" onClick={handleDelete}>Delete</button>
         
-//         </div>
-//       </div>
+        </div>
+      </div>
      
-//     </div>
-//   );
-// }
-// export default Product;
+    </div>
+  );
+}
+export default Product;

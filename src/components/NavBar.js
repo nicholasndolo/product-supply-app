8d2e2adce2a93661172c5d574e8af2212426f1a7
Nicholas <nicholas.nmalinda@gmail.com>
2022-09-12 10:04:41 +0300

    created NavLinks to home and about in the NavLink component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,27 @@
 // import React from 'react';
 // import {  NavLink } from 'react-router-dom';
 
-// function Navbar (){
+function Navbar (){
     
-//     return (
-//         <nav className="navbar navbar-expand-lg navbar-light bg-secondary">
-//                         <NavLink
-//                         className="nav-link"
-//                         to="/"
-//                         exact
-//                         >
-//                            <h3>Home</h3>
-//                         </NavLink>
-//                         <NavLink
-//                         className="nav-link"
-//                         to="/about"
-//                         exact
-//                         >
-//                             <h3>About</h3>
-//                     </NavLink>
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-secondary">
+                        <NavLink
+                        className="nav-link"
+                        to="/"
+                        exact
+                        >
+                           <h3>Home</h3>
+                        </NavLink>
+                        <NavLink
+                        className="nav-link"
+                        to="/about"
+                        exact
+                        >
+                            <h3>About</h3>
+                    </NavLink>
     
 
-//         </nav>
-//     )}
+        </nav>
+    )}
 
-// export default Navbar;
+export default Navbar;
